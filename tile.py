d538a7907673ab54d5c90f029da12f81172fb1bc
bearinfestedwaters <bearinfestedwaters@gmail.com>
2020-06-22 22:24:05 -0400

    Updated tile.py to properly use instance variables.

--- tile.py
+++ tile.py
@@ -1,14 +1,11 @@
 import pygame
 class Tile:
-    #contains needed information for instantiating individual tiles on grid
-    tileX = 0
-    tileY = 0
-    tileImg = None
-
-    tile_bonus = 0
+    
     def __init__(self, x, y):
         self.tileX = x
         self.tileY = y
+        self.tile_bonus = 0
+        self.tileImg = pygame.image.load('Images\\Tiles\\unfound_tile.png')
     def showTile(self,screen):
         screen.blit(self.tileImg, (self.tileX, self.tileY))
         print("show")
@@ -16,17 +13,23 @@ class Tile:
 
 class Plane(Tile):
     #Plane style of tile
-    #Plane image
-    tileImg = pygame.image.load('Images\\Tiles\\grass_tile.png')
+    def __init__(self, x, y):
+        Tile.__init__(self,x,y)
+        #Plane image
+        self.tileImg = pygame.image.load('Images\\Tiles\\grass_tile.png')
 
 class Water(Tile):
     #Water style of tile
-    #Water image
-    tileImg = pygame.image.load('Images\\Tiles\\water_tile.png')
+    def __init__(self, x, y):
+        Tile.__init__(self,x,y)
+        #Water image
+        self.tileImg = pygame.image.load('Images\\Tiles\\water_tile.png')
 
 class Forest(Tile):
     #Forest style of tile
-    #Forest image
-    tileImg = pygame.image.load('Images\\Tiles\\forest_tile.png')
-    tile_bonus = .25
+    def __init__(self, x, y):
+        Tile.__init__(self,x,y)
+        #Forest image
+        self.tileImg = pygame.image.load('Images\\Tiles\\forest_tile.png')
+        self.tile_bonus = .25
     
