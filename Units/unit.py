f5a2b978678ae3119a89b17c9a5c7d820ce19e02
Aaron T <aaron.taube@gmail.com>
2020-06-28 12:14:26 -0400

    Added dicts of strengths to unit subclasses Began work on unit management in Group class.

--- Units/unit.py
+++ Units/unit.py
@@ -1,27 +1,61 @@
 import pygame
 
 class Unit:
-    unitX = 0
-    unitY = 0
-    unit_type = "unknown"
+    #Base class for soldiers
+    def __init__(self):
+        self.unitX = 0
+        self.unitY = 0
+        self.unit_type = "unknown"
 
 class Axe(Unit):
-    unit_type = "axe"
+    #subclass for Axe soldiers
+    def __init__(self):
+        Unit.__init__(self)
+        self.unit_type = "axe"
+        #dict to show which units this soldier gets higher victory chances against
+        self.strengths = {
+            "axe": False,
+            "spear": True,
+            "sword": False,
+            "bow": True
+        }
 
 
 class Spear(Unit):
-    unit_type = "spear"
-
+    #subclass for Spear soldiers
+    def __init__(self):
+        Unit.__init__(self)
+        self.unit_type = "spear"
+        #dict to show which units this soldier gets higher victory chances against
+        self.strengths = {
+            "axe": False,
+            "spear": False,
+            "sword": True,
+            "bow": True
+        }
 class Sword(Unit):
-    unit_type = "sword"
-
+    #subclass for Sword soldiers
+    def __init__(self):
+        Unit.__init__(self)
+        self.unit_type = "sword"
+        #dict to show which units this soldier gets higher victory chances against
+        self.strengths = {
+            "axe": True,
+            "spear": False,
+            "sword": False,
+            "bow": True
+        }
 class Group:
     #collection of units located in a tile
     def __init__(self):
-        self.units = []    
+        self.units = []
+        self.count = 0    
     def add_unit(self, unit):
-        self.units.append(unit)
+        if self.count < 9:
+            self.count = self.count + 1
+            self.units.append(unit)
     def subtract_unit(self, unit):
         self.units.pop()
+        self.count = self.count - 1
     def show_group(self,screen):
         print("made it")
